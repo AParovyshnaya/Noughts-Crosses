e687f14597567695f295c4a895d3c3661524e34f
AParovyshnaya <66070466+AParovyshnaya@users.noreply.github.com>
2021-06-13 14:22:00 +0300

    #2 Поле создаётся

--- js/create.js
+++ js/create.js
@@ -1,3 +1,20 @@
-function hello() {
-    console.log("Hello world!")
+function whatsField() {
+    let field = document.getElementById("select_size").value;
+    console.log("hello");
+    create();
+}
+
+function create() {
+    let target = document.getElementById("field");
+    for (let i in [0, 1, 2]) {
+        let p = document.createElement("p");
+        p.setAttribute("class", "line");
+        for (let i in [0, 1, 2,]) {
+            let square = document.createElement("input");
+            square.setAttribute("class", "line");
+            square.setAttribute("type", "button");
+            p.appendChild(square);
+        }
+        target.appendChild(p);
+    }
 }
