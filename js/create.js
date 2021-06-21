7e41dfe9592e8270fa37df372055dc4d799b99e3
AParovyshnaya <66070466+AParovyshnaya@users.noreply.github.com>
2021-06-21 18:05:24 +0300

    #4

--- js/create.js
+++ js/create.js
@@ -1,18 +1,45 @@
 function whatsField() {
-    let field = document.getElementById("select_size").value;
-    create([0, 1, 2], [0, 1, 2]);
+    let size = document.getElementById("select_size").value;
+    if (size == "3x3") {
+        create(["0", "1", "2"], ["0", "1", "2"]);
+    }
 }
 
 function create(lines, inLines) {
-    let target = document.getElementById("field");
-    for (let i in lines) {
+    let target = deleteOld();
+    console.log(target);
+    for (let nubmersline in lines) {
         let line = document.createElement("div");
         line.setAttribute("class", "line");
+        iLine = nubmersline
         for (let i in inLines) {
             let square = document.createElement("div");
             square.setAttribute("class", "square");
+            square.setAttribute("id", "square"+ iLine + "-"+ i)
             line.appendChild(square);
         }
         target.appendChild(line);
     }
 }
+
+function deleteOld() {
+    let old = document.getElementById("field");
+    let body = document.getElementsByTagName("body")[0];
+    if (old != null) {
+        body.removeChild(old);
+    }
+    let newTarget = document.createElement("div");
+    newTarget.setAttribute("id", "field");
+    body.appendChild(newTarget);
+    return newTarget;
+}
+
+squares = document.getElementsByClassName("square");
+isFirst = true;
+
+squares.onclick function() {
+    if (isFirst) {
+        play();
+    }
+}
+
