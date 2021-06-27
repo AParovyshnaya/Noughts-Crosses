b42cb5fbf8ee972634b7b15fd5bdf5729f7594a4
AParovyshnaya <66070466+AParovyshnaya@users.noreply.github.com>
2021-06-27 11:44:43 +0300

    #6
    Порядок действительно проверяется

--- js/play.js
+++ js/play.js
@@ -1,15 +1,23 @@
 function whatsSquare(canPlay) {
     let squares = document.querySelectorAll(".square");
     console.log(squares);
+    let turn = "крестика (x)";
+    howsTurn(turn);
     for (let square in squares) {
         squares[square].onclick = function (e) {
             console.log('event triggered on element', e, e.target);
-            id(e.target);
+            howsTurn(turn);
+            if (turn == "крестика (x)") {
+                turn = "нолика (o)";
+            } else {
+                turn = "крестика (x)"
+            }
         };
         console.log("listener added to ", square);
     }
 }
 
-function id(square) {
-    console.log(square.id)
+function howsTurn(turn) {
+    target = document.getElementById("move");
+    target.textContent = "Сейчас ход " + turn;
 }
