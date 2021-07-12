a58bf0f3b375765d23354366252571e4777c7119
AParovyshnaya <66070466+AParovyshnaya@users.noreply.github.com>
2021-07-12 12:55:30 +0300

    #7 Пункт первый

--- js/play.js
+++ js/play.js
@@ -1,10 +1,13 @@
 function whatsSquare(canPlay) {
     let squares = document.querySelectorAll(".square");
     let turn = "крестика (x)";
+    let whatsturn = 0;
     howsTurn(null, turn);
     for (let square in squares) {
         squares[square].onclick = function (e) {
             turn = howsTurn(e.target, turn);
+            howWin(e.target, turn);
+            whatsturn += 1;
         };
     }
 }
@@ -27,8 +30,35 @@ function drawInTheSquare(square, turn) {
     let image = document.createElement("img");
     if (turn == "крестика (x)") {
         image.setAttribute("src", "images/x.svg");
+        square.setAttribute("data", "cross");
     } else {
         image.setAttribute("src", "images/o.svg");
+        square.setAttribute("data", "nolik");
     }
     square.appendChild(image);
+}
+
+function howWin(square) {
+    getThisType(square);
+}
+
+function getThisType(square) {
+    let thisType = [];
+    let type = square.getAttribute("data");
+    let all = document.querySelectorAll(".square");
+    let length = all.length;
+    let whenStop = 0;
+    for (let number in all) {
+        let element = all[number];
+        if (element.hasAttribute("data")) {
+            if (element.getAttribute("data") == type) {
+                thisType.push(element);
+            }
+        }
+        whenStop += 1;
+        if (whenStop == length) {
+            break;
+        }
+    }
+    console.log(thisType);
 }
