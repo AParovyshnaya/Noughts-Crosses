d88e43bb510c9092b4b3555bb71cc58fd5990bc5
AParovyshnaya <66070466+AParovyshnaya@users.noreply.github.com>
2021-08-04 15:00:35 +0300

    #14
    теперь соеденено

--- js/create.js
+++ js/create.js
@@ -1,21 +1,23 @@
 function whatsField() {
     let size = document.getElementById("select_size").value;
     if (size == "3x3") {
-        return create(["0", "1", "2"], ["0", "1", "2"], `square_3`);
+        return create(["0", "1", "2"], `3`);
     } else if (size == "19x19") {
-        return create(["0", "1", "2", `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`, `11`, `12`, `13`, `14`, `15`, `16`, `17`, `18`], ["0", "1", "2", `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`, `11`, `12`, `13`, `14`, `15`, `16`, `17`, `18`], `square_19`);
+        return create(["0", "1", "2", `3`, `4`, `5`, `6`, `7`, `8`, `9`, `10`, `11`, `12`, `13`, `14`, `15`, `16`, `17`, `18`], `19`);
     }
 }
 
-function create(lines, inLines, classSquare) {
+function create(lines, squaresSize) {
     let target = deleteOld("field");
     deleteOld("friends");
     deleteOld("new_game_target");
+    let [classLine, classSquare] = whatsClass(squaresSize);
+    console.log(classLine);
     for (let nubmersline in lines) {
         let line = document.createElement("div");
-        line.setAttribute("class", "line");
+        line.setAttribute("class", classLine);
         iLine = nubmersline
-        for (let i in inLines) {
+        for (let i in lines) {
             let square = document.createElement("div");
             square.setAttribute("class", classSquare);
             square.setAttribute("id", "square" + iLine + "-" + i);
@@ -40,5 +42,13 @@ function deleteOld(id) {
         let parent = document.getElementById("play_aktivity");
         parent.appendChild(newTarget);
         return newTarget;
+    }
+}
+
+function whatsClass(squaresSize) {
+    if (squaresSize == `3`) {
+        return [`line_3`, `square_3`];
+    } else {
+        return [`line_19`, `square_19`];
     }
 }
