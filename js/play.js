d85ff8da557f84cf11727172532cfc421f90ca36
AParovyshnaya <66070466+AParovyshnaya@users.noreply.github.com>
2021-08-18 15:38:39 +0300

    #22 Я умею определять тип.

--- js/play.js
+++ js/play.js
@@ -1,4 +1,11 @@
 function whatsSquare(classSquare) {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+
+        alert(`Вы используете мобильное устройство (телефон или планшет).`);
+
+    } else { 
+        alert(`Вы используете ПК.`); 
+    }
     let squares = document.querySelectorAll(classSquare);
     let turn = "крестика (x)";
     let whatsturn = 0;
